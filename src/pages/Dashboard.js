cd130bb473364df1762f86dff58a405dffd96b5d
shijing0628 <brendaliu0628@gmail.com>
2021-02-07 11:52:05 -0600

    edit bg of dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import DashboardTable from '../components/DashboardTable/DashboardTable'
-
+import ParticlesBg from 'particles-bg'
 function Dashboard() {
  return (
   <div>
+   <ParticlesBg type="circle" bg={true} id="particles-js" color="#E3D0FF" />
    <DashboardTable />
   </div>
  )
